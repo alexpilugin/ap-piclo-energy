15894546ad00d36cb7ddc1922274335fd8349b36
Alex Pilugin <alex.pilugin@microsec.co.uk>
2021-06-25 18:33:51 +0100

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="true" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer fixed app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
